bccc34ce8db816dc11b177e081a5aa695ba1f59a
a31r <desakedungpring@gmail.com>
2013-05-25 22:34:59 +0700

    Belajar dynamic-variable dengan conditional

--- user/a3ir/TODO/dynamic-variable-with-condition/dynamic-variable-with-condition.sh
+++ user/a3ir/TODO/dynamic-variable-with-condition/dynamic-variable-with-condition.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 
+#JumlahPaket=1773;
+JumlahPaket=$(dpkg --get-selections | wc -l);
+echo $JumlahPaket;
+
+if [[ $JumlahPaket -gt 1000 ]]; then
+	echo "Jumlah paket yang ter-instal sangat banyak"
+	echo "Disarankan untuk membuang paket - paket yang tidak perlu"
+fi
